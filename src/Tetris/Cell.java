75463370ffee77a658ed08191e8cc3aed1e155cc
Heavylife <15196614291@163.com>
2017-06-11 11:41:47 +0800

    one

---
+++ src/Tetris/Cell.java
@@ -0,0 +1,25 @@
+package Tetris;
+
+
+public class Cell {
+	int row;//行号
+	int col;//列号
+	public void drop(){//下落方法
+		row++;
+	}
+	public void moveLeft(){//左移col列
+		col--;
+	}
+	public void moveLeft(int col){//左移col列
+		this.col-=col;
+	}
+	public void moveRight(){//下落一行
+		col++;
+	}
+	public void moveRight(int col){//下落col行 ，方法的重载
+		this.col += col;
+	}
+	public String getCellInfo(){//获取格子坐标
+		return row+","+col; 
+	} 
+}
---
+++ src/Tetris/CellGame.java
@@ -0,0 +1,51 @@
+package Tetris;
+import java.util.Scanner;
+public class CellGame {
+	public static void main(String args[]){
+		Cell cell = new Cell();
+		cell.row = 3;
+		cell.col = 3;
+		int inPut;
+		printCell(cell);
+		do{//循环
+			System.out.println("请输入操作项目：");
+			Scanner sc = new Scanner(System.in);
+			inPut =  sc.nextInt();
+			fun(inPut,cell);
+			printCell(cell);
+		}while(inPut != 0);
+		System.out.println("游戏结束！");
+
+	}
+	public static void printCell(Cell cell){//打印墙及坐标
+		int totalRow = 20;
+		int totalCol = 10;//总的行和列
+		System.out.println("cell的位置为："+cell.getCellInfo());
+
+		for(int i = 0 ; i < totalRow;i++){
+			for(int j = 0;j<totalCol;j++){
+				if(i == cell.row && j == cell.col){
+					System.out.print("* ");
+				}else{
+					System.out.print("- ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("1 -- 下落，2 -- 向左，3 -- 向右，0 -- 退出");
+	}
+	public static void fun(int num,Cell cell){//移动操作
+		switch(num){
+		case 1:
+			cell.drop();
+			break;
+		case 2:
+			cell.moveLeft();
+			break;
+		case 3:
+			cell.moveRight();
+			break;
+		
+		}
+	}
+}
