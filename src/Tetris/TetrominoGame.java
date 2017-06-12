44a699aa09b4dc17146f2c298995332a42f85dbb
Heavylife <15196614291@163.com>
2017-06-12 22:12:31 +0800

    3

---
+++ src/Tetris/TetrominoGame.java
@@ -0,0 +1,19 @@
+package Tetris;
+
+public class TetrominoGame {
+	public static void main(String args[]){
+		TetrominoT t = new TetrominoT(3,4);
+		t.print();
+		
+	}
+	public static void printCell(){
+		
+		int totalRow = 20;
+		int totalCol =10;
+		for(int i = 0;i<totalRow;i++){
+			for(int j = 0;j<totalCol;j++){
+				
+			}
+		}
+	}
+}
