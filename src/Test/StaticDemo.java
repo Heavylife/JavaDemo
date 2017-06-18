c71daaabbe8b61b2a9716cb73469eb459555da56
Heavylife <15196614291@163.com>
2017-06-18 09:23:31 +0800

    d

--- src/Test/StaticDemo.java
+++ src/Test/StaticDemo.java
@@ -8,10 +8,12 @@ public class StaticDemo {
 	public static void show(String str){
 		System.out.println("我是重载的静态方法");
 	}
+	 static String str;
 	//静态方法
 	public static void main(String[] args) {
+		str = "静态变量";
 		StaticDemo.show("静态方法类名调用");
-		show("静态方法直接调用");
+		show("静态方法直接调用"+str);
 		StaticDemo s = new StaticDemo();
 		s.show();
 		s.print();
@@ -21,7 +23,7 @@ public class StaticDemo {
 	public void print(){
 		System.out.println("普通方法调用");
 		show();
-		show("静态方法直接调用");
+		show("普通方法直接调用"+str);
 		StaticDemo.show("静态方法类名调用");
 	}
 }
